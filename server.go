708f4b70c96e183a134b8c642a4b46528ca0a60f
asuka <yagi.asuka.0x06@gmail.com>
2026-02-23 22:19:49 +0900

    feat: show request log

--- server.go
+++ server.go
@@ -1,15 +1,29 @@
 package server
 
 import (
+	"log/slog"
 	"net/http"
 
 	"github.com/a-skua/mock-server/handler"
 )
 
+type RequestLog struct {
+	next http.Handler
+}
+
+func NewRequestLog(next http.Handler) *RequestLog {
+	return &RequestLog{next: next}
+}
+
+func (l *RequestLog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	slog.Info("Received request", "method", r.Method, "path", r.URL.Path)
+	l.next.ServeHTTP(w, r)
+}
+
 func NewServeMux() *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", handler.Hello)
-	mux.HandleFunc("/sleep", handler.Sleep)
-	mux.HandleFunc("/notfound", handler.NotFound)
+	mux.Handle("/", NewRequestLog(http.HandlerFunc(handler.Hello)))
+	mux.Handle("/sleep", NewRequestLog(http.HandlerFunc(handler.Sleep)))
+	mux.Handle("/notfound", NewRequestLog(http.HandlerFunc(handler.NotFound)))
 	return mux
 }
